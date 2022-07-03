be95fe556cd5972e19ff7ce871ffe8320e661a2c
lcydr <1211753823@qq.com>
2022-07-03 20:38:07 +0800

    完成添加任务

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="todoapp">
     <!-- 除了驼峰, 还可以使用-转换链接 -->
-    <TodoHeader></TodoHeader>
+    <TodoHeader @btnAdd="btnAddFn"></TodoHeader>
     <TodoMain :list="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </section>
@@ -21,10 +21,22 @@ export default {
     return {
       list: [
         { id: 100, name: "吃饭", isDone: true },
-        { id: 201, name: "睡觉", isDone: false },
-        { id: 103, name: "打豆豆", isDone: true },
+        { id: 101, name: "睡觉", isDone: false },
+        { id: 102, name: "打豆豆", isDone: true },
       ],
     };
+  },
+  methods: {
+    btnAddFn(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
+        id,
+        name: val,
+        isDone: false,
+      });
+    },
   },
   components: {
     TodoHeader,
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -4,10 +4,31 @@
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all"></label>
     <!-- label 可以关联一个表单标签 -->
-    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
+    <input
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+      @keydown.enter="btnAdd"
+      v-model="task"
+    />
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      task: "",
+    };
+  },
+  methods: {
+    btnAdd() {
+      if (this.task.trim().length === 0) {
+        return alert("请输入完整信息");
+      }
+      this.$emit("btnAdd", this.task);
+      this.task = "";
+    },
+  },
+};
 </script>
